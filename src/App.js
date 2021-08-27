f6d81f351890a02d2283059bc9cb260b175fa52c
Rick <rmansfield@realpeoplesratings.com>
2021-08-27 16:51:38 -0500

    Add Router to Index.js and Start Home page.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Login from './components/Login'
 import Logout from './components/Logout'
+import Home from './components/Home'
 function App() {
   return (
     <div className="App">
       <Login />
       <Logout />
+      <Home />
     </div>
   );
 }
