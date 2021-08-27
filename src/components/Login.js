dbff7aca6d25ec125f712aa14601ae1fb4469214
Dharmik Savaliya <danny@Dharmiks-MacBook-Pro.local>
2021-08-27 14:24:09 -0700

    Login and axios working

---
+++ src/components/Login.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+// const credentials = {
+//     username: "",
+//     password: ""
+// }
+const Login = ()=>{
+    const [value, setValue]= useState({username: "",
+    password: ""})
+
+    const handleChange = e => {
+        setValue({
+              ...value,
+            [e.target.name]: e.target.value
+        });
+      };
+      console.log(value)
+     const login = e => {
+        e.preventDefault(); 
+        axios.post('https://potluck-planner-2.herokuapp.com/api/auth/login', value)
+          .then(res => {
+              console.log(res)
+            localStorage.setItem("token", res.data.token);
+         
+            // this.props.history.push('/friends')
+          })
+          .catch(err=> {
+            console.log(err);
+          });
+      };
+return(
+    <div>
+        <form onSubmit={login}>
+          <input
+            type="text"
+            name="username"
+            value={value.username}
+            onChange={handleChange}
+            placeholder='Username'
+            style={{padding:'10px'}}
+          /><br/>
+          <input
+            type="password"
+            name="password"
+            value={value.password}
+            onChange={handleChange}
+            placeholder='Password'
+            style={{padding:'10px', margin:'10px'}}
+          /><br/>
+          <button style={{padding:'10px'}}>Log in</button>
+        </form>
+    </div>
+)
+}
+export default Login
